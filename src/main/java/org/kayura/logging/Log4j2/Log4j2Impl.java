3e098e20f3207afa7ea1662ef7cb517d4fccd2db
xialiang <liangxia@live.com>
2015-07-13 22:32:23 +0800

    Signed-off-by: xialiang <liangxia@live.com>

--- src/main/java/org/kayura/logging/Log4j2/Log4j2Impl.java
+++ src/main/java/org/kayura/logging/Log4j2/Log4j2Impl.java
@@ -1,51 +1,50 @@
 package org.kayura.logging.Log4j2;
 
 import org.kayura.logging.Log;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.spi.AbstractLogger;
 
 public class Log4j2Impl implements Log {
 
-	  private Log log;
+	private Log log;
 
-	  public Log4j2Impl(String clazz) {
-	    Logger logger = LogManager.getLogger(clazz);
+	public Log4j2Impl(String clazz) {
+		Logger logger = LogManager.getLogger(clazz);
 
-	    if (logger instanceof AbstractLogger) {
-	      log = new Log4j2AbstractLoggerImpl((AbstractLogger) logger);
-	    } else {
-	      log = new Log4j2LoggerImpl(logger);
-	    }
-	  }
+		if (logger instanceof AbstractLogger) {
+			log = new Log4j2AbstractLoggerImpl((AbstractLogger) logger);
+		} else {
+			log = new Log4j2LoggerImpl(logger);
+		}
+	}
 
-	  public boolean isDebugEnabled() {
-	    return log.isDebugEnabled();
-	  }
+	public boolean isDebugEnabled() {
+		return log.isDebugEnabled();
+	}
 
-	  public boolean isTraceEnabled() {
-	    return log.isTraceEnabled();
-	  }
+	public boolean isTraceEnabled() {
+		return log.isTraceEnabled();
+	}
 
-	  public void error(String s, Throwable e) {
-	    log.error(s, e);
-	  }
+	public void error(String s, Throwable e) {
+		log.error(s, e);
+	}
 
-	  public void error(String s) {
-	    log.error(s);
-	  }
+	public void error(String s, Object... args) {
+		log.error(String.format(s, args));
+	}
 
-	  public void debug(String s) {
-	    log.debug(s);
-	  }
+	public void debug(String s, Object... args) {
+		log.debug(String.format(s, args));
+	}
 
-	  public void trace(String s) {
-	    log.trace(s);
-	  }
+	public void trace(String s, Object... args) {
+		log.trace(String.format(s, args));
+	}
 
-	  public void warn(String s) {
-	    log.warn(s);
-	  }
+	public void warn(String s, Object... args) {
+		log.warn(String.format(s, args));
+	}
 
 }
--- src/main/java/org/kayura/logging/nologging/NoLoggingImpl.java
+++ src/main/java/org/kayura/logging/nologging/NoLoggingImpl.java
@@ -2,36 +2,34 @@ package org.kayura.logging.nologging;
 
 import org.kayura.logging.Log;
 
-
 /**
  * @author Clinton Begin
  */
 public class NoLoggingImpl implements Log {
 
-  public NoLoggingImpl(String clazz) {
-  }
+	public NoLoggingImpl(String clazz) {
+	}
 
-  public boolean isDebugEnabled() {
-    return false;
-  }
+	public boolean isDebugEnabled() {
+		return false;
+	}
 
-  public boolean isTraceEnabled() {
-    return false;
-  }
+	public boolean isTraceEnabled() {
+		return false;
+	}
 
-  public void error(String s, Throwable e) {
-  }
+	public void error(String msg, Throwable e) {
+	}
 
-  public void error(String s) {
-  }
+	public void error(String msg, Object... args) {
+	}
 
-  public void debug(String s) {
-  }
+	public void debug(String msg, Object... args) {
+	}
 
-  public void trace(String s) {
-  }
+	public void trace(String msg, Object... args) {
+	}
 
-  public void warn(String s) {
-  }
-
+	public void warn(String msg, Object... args) {
+	}
 }
