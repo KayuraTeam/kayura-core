1048984c6fb7c7739a00fa168568db248b736360
xialiang <liangxia@live.com>
2015-07-14 23:32:04 +0800

    Signed-off-by: xialiang <liangxia@live.com>

---
+++ src/main/java/org/kayura/utils/MapUtils.java
@@ -0,0 +1,15 @@
+/**
+ * Copyright 2015-2015 the original author or authors.
+ * HomePage: http://www.kayura.org
+ */
+package org.kayura.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author liangxia@live.com
+ */
+public class MapUtils {
+
+}
