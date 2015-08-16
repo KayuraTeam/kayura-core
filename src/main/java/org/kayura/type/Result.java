487a6e04b9563c3ba966481055a4d2aa480de3d1
liangxia <liangxia@live.com>
2015-08-16 15:46:07 +0800

    Signed-off-by: liangxia <liangxia@live.com>

--- src/main/java/org/kayura/type/Result.java
+++ src/main/java/org/kayura/type/Result.java
@@ -10,54 +10,66 @@ import java.io.Serializable;
  * @author liangxia@live.com
  */
 public class Result<E extends Serializable> implements Serializable {
-
-	private static final long serialVersionUID = 37468166158631349L;
-
-	private String message;
-	private Integer code;
-	private E data;
-
-	public Result() {
-		this.code = 1;
+    
+    private static final long serialVersionUID = 37468166158631349L;
+    
+    private StringBuilder message;
+    private Integer code;
+    private E data;
+    
+    public Result() {
+	this.message = new StringBuilder();
+	this.code = 1;
+    }
+    
+    public Result(Integer code) {
+	this();
+	this.code = code;
+    }
+    
+    public Result(Integer code, String message) {
+	this(code);
+	setMessage(message);
+    }
+    
+    public void setError(String message, Object... args) {
+	this.code = 0;
+	setMessage(message, args);
+    }
+    
+    public void setSuccess(String message, Object... args) {
+	this.code = 1;
+	setMessage(message, args);
+    }
+    
+    public Integer getCode() {
+	return code;
+    }
+    
+    public void setCode(Integer code) {
+	this.code = code;
+    }
+    
+    public String getMessage() {
+	return message.toString();
+    }
+    
+    public void setMessage(String message, Object... args) {
+	if (this.message != null && this.message.length() > 0) {
+	    this.message = new StringBuilder();
 	}
-
-	public Result(Integer code) {
-		this();
-		this.code = code;
-	}
-
-	public Result(Integer code, String message) {
-		this(code);
-		this.message = message;
-	}
-
-	public Result(Integer code, String message, E data) {
-		this(code);
-		this.message = message;
-		this.data = data;
-	}
-
-	public Integer getCode() {
-		return code;
-	}
-
-	public void setCode(Integer code) {
-		this.code = code;
-	}
-
-	public String getMessage() {
-		return message;
-	}
-
-	public void setMessage(String message, Object... args) {
-		this.message = String.format(message, args);
-	}
-
-	public E getData() {
-		return data;
-	}
-
-	public void setData(E data) {
-		this.data = data;
-	}
+	addMessage(message, args);
+    }
+    
+    public void addMessage(String message, Object... args) {
+	this.message.append(String.format(message, args));
+    }
+    
+    public E getData() {
+	return data;
+    }
+    
+    public void setData(E data) {
+	this.data = data;
+    }
 }
