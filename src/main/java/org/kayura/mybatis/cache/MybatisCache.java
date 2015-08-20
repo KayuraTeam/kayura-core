4cf65216ab77b92c957fa037554dde3d4bdde06a
liangxia <liangxia@live.com>
2015-08-20 18:32:02 +0800

    Signed-off-by: liangxia <liangxia@live.com>

--- src/main/java/org/kayura/mybatis/cache/MybatisCache.java
+++ src/main/java/org/kayura/mybatis/cache/MybatisCache.java
@@ -14,49 +14,48 @@ import java.util.concurrent.locks.ReadWriteLock;
  *
  */
 public class MybatisCache implements Cache {
-    
-    private org.apache.ibatis.cache.Cache cache;
-    
-    public MybatisCache(Class<? extends org.apache.ibatis.cache.Cache> implClass) {
-	try {
-	    Constructor<? extends org.apache.ibatis.cache.Cache> candidate = implClass
-		    .getConstructor();
-	    cache = candidate.newInstance();
-	} catch (Exception e) {
+
+	private org.apache.ibatis.cache.Cache cache;
+
+	public MybatisCache(Class<? extends org.apache.ibatis.cache.Cache> implClass) {
+		try {
+			Constructor<? extends org.apache.ibatis.cache.Cache> candidate = implClass.getConstructor();
+			cache = candidate.newInstance();
+		} catch (Exception e) {
+		}
 	}
-    }
-    
-    @SuppressWarnings("unused")
-    private MybatisCache() {
-	// ...
-    }
-    
-    public String getId() {
-	return cache.getId();
-    }
-    
-    public void put(Object key, Object value) {
-	cache.putObject(key, value);
-    }
-    
-    public Object get(Object key) {
-	return cache.getObject(key);
-    }
-    
-    public Object remove(Object key) {
-	return cache.removeObject(key);
-    }
-    
-    public void clear() {
-	cache.clear();
-    }
-    
-    public int getSize() {
-	return cache.getSize();
-    }
-    
-    public ReadWriteLock getReadWriteLock() {
-	return cache.getReadWriteLock();
-    }
-    
+
+	@SuppressWarnings("unused")
+	private MybatisCache() {
+		// ...
+	}
+
+	public String getId() {
+		return cache.getId();
+	}
+
+	public void put(Object key, Object value) {
+		cache.putObject(key, value);
+	}
+
+	public Object get(Object key) {
+		return cache.getObject(key);
+	}
+
+	public Object remove(Object key) {
+		return cache.removeObject(key);
+	}
+
+	public void clear() {
+		cache.clear();
+	}
+
+	public int getSize() {
+		return cache.getSize();
+	}
+
+	public ReadWriteLock getReadWriteLock() {
+		return cache.getReadWriteLock();
+	}
+
 }
--- src/main/java/org/kayura/mybatis/type/PropertiesTypeHandler.java
+++ src/main/java/org/kayura/mybatis/type/PropertiesTypeHandler.java
@@ -14,53 +14,53 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class PropertiesTypeHandler extends BaseTypeHandler<Properties> {
-    
-    ObjectMapper objectMapper = new ObjectMapper();
-    
-    public PropertiesTypeHandler() {
-    }
-    
-    @Override
-    public void setNonNullParameter(PreparedStatement ps, int i, Properties parameter,
-	    JdbcType jdbcType) throws SQLException {
-	String s;
-	try {
-	    s = objectMapper.writeValueAsString(parameter);
-	    ps.setString(i, s);
-	} catch (JsonProcessingException e) {
-	    e.printStackTrace();
+
+	ObjectMapper objectMapper = new ObjectMapper();
+
+	public PropertiesTypeHandler() {
 	}
-    }
-    
-    private Properties getJsonObject(String json) {
-	Properties o = null;
-	try {
-	    if (json != null && json != "") {
-		o = (Properties) objectMapper.readValue(json, Properties.class);
-	    }
-	} catch (JsonProcessingException e) {
-	    e.printStackTrace();
-	} catch (IOException e) {
-	    e.printStackTrace();
+
+	@Override
+	public void setNonNullParameter(PreparedStatement ps, int i, Properties parameter, JdbcType jdbcType)
+			throws SQLException {
+		String s;
+		try {
+			s = objectMapper.writeValueAsString(parameter);
+			ps.setString(i, s);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
 	}
-	return o;
-    }
-    
-    @Override
-    public Properties getNullableResult(ResultSet rs, String columnName) throws SQLException {
-	String json = rs.getString(columnName);
-	return getJsonObject(json);
-    }
-    
-    @Override
-    public Properties getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
-	String json = rs.getString(columnIndex);
-	return getJsonObject(json);
-    }
-    
-    @Override
-    public Properties getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
-	String json = cs.getString(columnIndex);
-	return getJsonObject(json);
-    }
+
+	private Properties getJsonObject(String json) {
+		Properties o = null;
+		try {
+			if (json != null && json != "") {
+				o = (Properties) objectMapper.readValue(json, Properties.class);
+			}
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return o;
+	}
+
+	@Override
+	public Properties getNullableResult(ResultSet rs, String columnName) throws SQLException {
+		String json = rs.getString(columnName);
+		return getJsonObject(json);
+	}
+
+	@Override
+	public Properties getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
+		String json = rs.getString(columnIndex);
+		return getJsonObject(json);
+	}
+
+	@Override
+	public Properties getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
+		String json = cs.getString(columnIndex);
+		return getJsonObject(json);
+	}
 }
