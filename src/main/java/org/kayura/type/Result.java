c78ae1964366d8cd053e390b3d0a9daa8a777d63
liangxia <liangxia@live.com>
2015-09-14 23:24:48 +0800

    Signed-off-by: liangxia <liangxia@live.com>

--- src/main/java/org/kayura/type/Result.java
+++ src/main/java/org/kayura/type/Result.java
@@ -10,66 +10,73 @@ import java.io.Serializable;
  * @author liangxia@live.com
  */
 public class Result<E extends Serializable> implements Serializable {
-    
-    private static final long serialVersionUID = 37468166158631349L;
-    
-    private StringBuilder message;
-    private Integer code;
-    private E data;
-    
-    public Result() {
-	this.message = new StringBuilder();
-	this.code = 1;
-    }
-    
-    public Result(Integer code) {
-	this();
-	this.code = code;
-    }
-    
-    public Result(Integer code, String message) {
-	this(code);
-	setMessage(message);
-    }
-    
-    public void setError(String message, Object... args) {
-	this.code = 0;
-	setMessage(message, args);
-    }
-    
-    public void setSuccess(String message, Object... args) {
-	this.code = 1;
-	setMessage(message, args);
-    }
-    
-    public Integer getCode() {
-	return code;
-    }
-    
-    public void setCode(Integer code) {
-	this.code = code;
-    }
-    
-    public String getMessage() {
-	return message.toString();
-    }
-    
-    public void setMessage(String message, Object... args) {
-	if (this.message != null && this.message.length() > 0) {
-	    this.message = new StringBuilder();
+
+	private static final long serialVersionUID = 37468166158631349L;
+
+	private StringBuilder message = new StringBuilder();
+	private Integer code = 1;
+	private E data;
+
+	public Result() {
 	}
-	addMessage(message, args);
-    }
-    
-    public void addMessage(String message, Object... args) {
-	this.message.append(String.format(message, args));
-    }
-    
-    public E getData() {
-	return data;
-    }
-    
-    public void setData(E data) {
-	this.data = data;
-    }
+
+	@Override
+	public String toString() {
+		return String.format("Code: %d; Message: %s", code, message.toString());
+	}
+
+	public Result(Integer code) {
+		this();
+		this.code = code;
+	}
+
+	public Result(Integer code, String message) {
+		this(code);
+		setMessage(message);
+	}
+
+	public void setError(String message, Object... args) {
+		this.code = 0;
+		setMessage(message, args);
+	}
+
+	public void setSuccess(String message, Object... args) {
+		this.code = 1;
+		setMessage(message, args);
+	}
+
+	public Integer getCode() {
+		return code;
+	}
+
+	public void setCode(Integer code) {
+		this.code = code;
+	}
+
+	public String getMessage() {
+		return message.toString();
+	}
+
+	public void setMessage(String message, Object... args) {
+		if (this.message != null && this.message.length() > 0) {
+			this.message = new StringBuilder();
+		}
+		addMessage(message, args);
+	}
+
+	public void addMessage(String message, Object... args) {
+		if (args.length > 0) {
+			this.message.append(String.format(message, args));
+		} else {
+			this.message.append(message);
+		}
+	}
+
+	public E getData() {
+		return data;
+	}
+
+	public void setData(E data) {
+		this.data = data;
+	}
 }
